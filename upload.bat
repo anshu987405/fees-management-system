9d45f2279fd761b015f4bb3fd028164b69069ea7
anshu sharma <anshusharma987405@gmail.com>
2026-05-03 13:36:56 +0530

    auto update deploy

--- upload.bat
+++ upload.bat
@@ -1,16 +1,33 @@
 @echo off
 echo ===============================
-echo   PUSHING CODE TO GITHUB
+echo   FEES MANAGEMENT AUTO DEPLOY
 echo ===============================
 
+echo.
+echo 🔁 Step 1: Adding changes...
 git add .
-git commit -m "auto update"
+
+echo.
+echo 📝 Step 2: Commiting...
+git commit -m "auto update deploy"
+
+echo.
+echo 🚀 Step 3: Pushing to GitHub...
 git push
 
+echo.
 echo ===============================
 echo   DONE ✅
 echo ===============================
 echo.
-echo 👉 Now go to Render and click:
-echo Manual Deploy -> Deploy latest commit
+
+echo ⚠️ IMPORTANT:
+echo 👉 Ab Render me jao:
+echo 👉 Manual Deploy -> Deploy latest commit
+echo.
+
+echo 🌐 Test URL:
+echo https://fees-management-system-i8sj.onrender.com/api
+echo.
+
 pause
