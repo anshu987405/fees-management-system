a42bab2a3bb599857807da0734d72d6fe0a48478
anshu sharma <anshusharma987405@gmail.com>
2026-05-03 13:31:52 +0530

    auto update

--- upload.bat
+++ upload.bat
@@ -1,44 +1,16 @@
 @echo off
-echo ==============================
-echo   FeesPro Auto Git Upload
-echo ==============================
+echo ===============================
+echo   PUSHING CODE TO GITHUB
+echo ===============================
 
-:: Check git installed
-git --version >nul 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    echo Git install nahi hai. Pehle Git install karo.
-    pause
-    exit
-)
-
-echo.
-echo Step 1: Git init...
-git init
-
-echo.
-echo Step 2: Adding files...
 git add .
+git commit -m "auto update"
+git push
 
+echo ===============================
+echo   DONE ✅
+echo ===============================
 echo.
-echo Step 3: Commit...
-git commit -m "Auto upload"
-
-echo.
-echo Step 4: Set branch...
-git branch -M main
-
-echo.
-echo Step 5: Connect repo...
-git remote remove origin >nul 2>&1
-git remote add origin https://github.com/anshu987405/fees-management-system.git
-
-echo.
-echo Step 6: Push to GitHub...
-git push -u origin main
-
-echo.
-echo ==============================
-echo   Upload Complete ✅
-echo ==============================
-
+echo 👉 Now go to Render and click:
+echo Manual Deploy -> Deploy latest commit
 pause
